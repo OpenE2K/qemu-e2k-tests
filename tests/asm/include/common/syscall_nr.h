a7aa47c752fd0eab1290ec9541118cc4b8649118
Denis Drakhnia <numas13@gmail.com>
2024-02-28 20:49:55 +0200

    asm/syscall: add simple fast syscall test

--- tests/asm/include/common/syscall_nr.h
+++ tests/asm/include/common/syscall_nr.h
@@ -1,11 +1,19 @@
 #ifndef COMMON_SYSCALL_NR_H
 #define COMMON_SYSCALL_NR_H
 
-#define NR_exit                   1
-#define NR_write                  4
-#define NR_mmap                  90
+#define NR_exit                 1
+#define NR_write                4
+#define NR_gettimeofday         78
+#define NR_mmap                 90
 #define NR_rt_sigaction         174
 #define NR_rt_sigreturn         173
 #define NR_access_hw_stacks     254
+
+#define NR_fast_sys_gettimeofday      0
+#define NR_fast_sys_clock_gettime     1
+#define NR_fast_sys_getcpu            2
+#define NR_fast_sys_siggetmask        3
+#define NR_fast_sys_getcontext        4
+#define NR_fast_sys_set_return        5
 
 #endif // COMMON_SYSCALL_NR_H
